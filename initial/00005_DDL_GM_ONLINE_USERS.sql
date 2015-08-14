5f1b094d11b437b00fa1493fa2362c0f82fa1856
Man Wei Tam <manweitam@gmail.com>
2015-08-14 00:03:09 -0500

    initial version

---
+++ initial/00005_DDL_GM_ONLINE_USERS.sql
@@ -0,0 +1,25 @@
+alter session set current_schema=gm_apex;
+CREATE TABLE  "GM_ONLINE_USERS" 
+   (	"ONLINE_USER_ID" NUMBER, 
+	"USERNAME" NVARCHAR2(50), 
+	"LOGIN_TIMESTAMP" TIMESTAMP (6), 
+	"LAST_PING_TIMESTAMP" TIMESTAMP (6), 
+	"SESSION_ID" NUMBER, 
+	 CONSTRAINT "GM_ONLINE_USERS_PK" PRIMARY KEY ("ONLINE_USER_ID")
+  USING INDEX  ENABLE
+   )
+/
+create sequence gm_online_users_seq;
+/
+CREATE OR REPLACE TRIGGER  "BI_GM_ONLINE_USERS" 
+  before insert on "GM_ONLINE_USERS"               
+  for each row  
+begin   
+  if :NEW."ONLINE_USER_ID" is null then 
+    select "GM_ONLINE_USERS_SEQ".nextval into :NEW."ONLINE_USER_ID" from sys.dual; 
+  end if; 
+end; 
+
+/
+ALTER TRIGGER  "BI_GM_ONLINE_USERS" ENABLE
+/
