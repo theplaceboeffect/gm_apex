1deb0b06f63e2aa03265d4a41f2444286cbb00ee
Man Wei Tam <manweitam@gmail.com>
2015-09-02 23:26:25 -0500

    Better time_ago function.

--- initial/00001_DDL_UTIL.sql
+++ initial/00001_DDL_UTIL.sql
@@ -6,8 +6,19 @@ end GM_UTIL;
 /
 create or replace package body GM_UTIL as
   function time_ago(dt date) return varchar2 as
+  n number;
+  s varchar(10);
   begin
-    return round((sysdate - dt)*1440) || ' mins ago';
+    n := round((sysdate - dt)*1440);
+    s := 'mins';
+    
+    if n = 0 then
+      n:=round((sysdate - dt)*18400);
+      s:= 'secs';
+    end if;
+    
+    return n || ' ' || s || ' ago.';
+    
   end time_ago;
 end GM_UTIL;
 /
